36ae893d5369dddea8e7a7e3156a32d6b0129688
LilikMurdiyah <83940396+LilikMurdiyah@users.noreply.github.com>
2021-05-14 22:23:58 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function f_wisata1(){
+    document.getElementById('wisata1').innerHTML="Di website Tambalkita, anda bisa mendapatkan berbagai bantuan seperti untuk memperbaiki motor anda, menambal ban sepeda motor, atau mobil anda serta memanggil montir handal yang berada di sekitar anda Dengan adanya website Tambalkita, dapat membantu anda jika terdapat musibah seperti ban kendaraan anda bocor disuatu tempat, serta jauh dari pemukiman, ataupun jauh dari bengkel website Tambalkita siap untuk membantunya"
+}
+function f_wisata2(){
+    document.getElementById('wisata2').innerHTML="Karena di website Tambalkita memiliki beberapa fitur yang unik diantaranya yakni terdapat kontak nomor sang mortir yang berfungsi untuk memudahkan komunikasi antara mortir dengan customer, fitur berikutnya terdapat fitur google maps yang berfungsi untuk melihat posisi tambal Ban terdekat"
+}
+function f_wisata3(){
+    document.getElementById('wisata3').innerHTML="Cara untuk order tambalkita yakni customer bisa mengunjungi website Tambalkita, setelah itu customer dapat memilih kontak montir yang telah disediakan, serta customer dapat membuka fitur maps didalam website untuk melihat poisisi tambal ban terdekat"
+}
+function f_wisata4(){
+    document.getElementById('wisata4').innerHTML="Untuk bisa menjadi mitra tambalkita dengan cara sang montir atau pemilik bengkel dapat menghubungi customer service yang ada didalam website tersebut serta melengkapi syarat-syarat yang telah ditentukan oleh customer service"
+}
+function f_hide1(){
+  document.getElementById('wisata1').style.display = 'none';
+}
+function f_hide2(){
+  document.getElementById('wisata2').style.display = 'none';
+}
+function f_hide3(){
+  document.getElementById('wisata3').style.display = 'none';
+}
+function f_hide4(){
+  document.getElementById('wisata4').style.display = 'none';
+}
